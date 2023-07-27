a8d69579b75647c437ad464776363892a72df721
Marimatsu-13 <124256399+Marimatsu-13@users.noreply.github.com>
2023-07-27 15:25:36 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+let backgroundVideo = document.querySelector("#background-video");
+let logo = document.querySelector(".logo");
+let banner = document.querySelector(".banner");
+
+banner.addEventListener("wheel", function parallax(){
+  backgroundVideo.style.top = - window.scrollY + "px";
+}
+)
+
 const swiper = new Swiper('.swiper', {
       effect: "coverflow",
       direction: "horizontal",
@@ -23,5 +32,13 @@ place.addEventListener('wheel', function scroll(){
   bigCloud.style.transform = "translateX(-300px)";
   smallCloud.style.transform = "translateX(-300px)";
 
+});
+
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".menu-navigation");
+
+burger.addEventListener("click", ()=>{
+burger.classList.toggle("active");
+menu.classList.toggle("active");
 });
 
